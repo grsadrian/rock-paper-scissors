5f87d981aaf4dcdeb728f22e9736f1aed00b80b1
Adrian Silva <grsadrian01@outlook.com>
2025-05-24 08:46:20 -0300

    feat: Adds function to play 5 times Adds a function to play 5 times and improves the playground function

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
+  // Give a random choice for the computer and return a string
   const choice = (Math.random() * 3).toFixed(0);
   if (choice <= 1) {
     return "Rock";
@@ -12,6 +10,7 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
+  // Get a human string choice and convert to a number
   let choice = prompt("rock, paper or scissor: ").toUpperCase();
   if (choice === "ROCK") {
     choice = 1;
@@ -25,8 +24,7 @@ function getHumanChoice() {
 }
 
 function playRound(computerChoice, humanChoice) {
-  let computerWin = false;
-  let humanWin = false;
+  // Transform the computer selection string to a number
   let computerSelection = 3;
   if (computerChoice === "Rock") {
     computerSelection = 1;
@@ -34,19 +32,41 @@ function playRound(computerChoice, humanChoice) {
     computerSelection = 2;
   }
 
+  // Revert the human selection to a string
+  let humanSelection = "Scissor";
+  if (humanChoice === 1) {
+    humanSelection = "Rock";
+  } else if (humanChoice === 2) {
+    humanSelection = "Paper";
+  }
+
+  // Compare the computer selection & human selection to determine a winner
   if (
     (computerSelection == 1 && humanChoice == 3) ||
     (computerSelection == 2 && humanChoice == 1) ||
     (computerSelection == 3 && humanChoice == 2)
   ) {
-    computerWin = true;
+    console.log(`You lose ! ${computerChoice} beats ${humanSelection}`);
+    computerScore++;
   } else if (computerSelection == humanChoice) {
     console.log("It's a tie");
   } else {
-    humanWin = true;
+    console.log(`You Win ! ${humanSelection} beats ${computerChoice}`);
+    humanScore++;
   }
 }
 
-const computerSelection = getComputerChoice();
-const humanSelection = getHumanChoice();
-playRound(computerSelection, humanSelection);
+// Loop for a 5 times play
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    const computerSelection = getComputerChoice();
+    const humanSelection = getHumanChoice();
+    playRound(computerSelection, humanSelection);
+  }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+playGame();
+console.log(`Human Score: ${humanScore}`);
+console.log(`Computer Score: ${computerScore}`);
