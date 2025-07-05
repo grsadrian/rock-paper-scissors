85bac15130c9327ae6607ad64d36c0a834686955
Adrian Silva <grsadrian01@outlook.com>
2025-07-05 13:45:33 -0300

    perf(app): refactor, clean and optimize the game flow

--- app.js
+++ app.js
@@ -1,72 +1,78 @@
+const menu = document.querySelector("#menu");
+
 function getComputerChoice() {
   // Give a random choice for the computer and return a string
   const choice = (Math.random() * 3).toFixed(0);
   if (choice <= 1) {
-    return "Rock";
+    return "rock";
   } else if (choice == 2) {
-    return "Paper";
+    return "paper";
   }
-  return "Scissor";
+  return "scissor";
 }
 
-function getHumanChoice() {
-  // Get a human string choice and convert to a number
-  // let choice = prompt("rock, paper or scissor: ").toUpperCase();
-  if (choice === "ROCK") {
-    choice = 1;
-    return choice;
-  } else if (choice === "PAPER") {
-    choice = 2;
-    return choice;
-  }
-  choice = 3;
+function getHumanChoice(event) {
+  // Get a human choice and convert to a number
+  let choice = event.target.classList[0];
   return choice;
 }
 
+menu.addEventListener("click", getHumanChoice);
+
 function playRound(computerChoice, humanChoice) {
   // Transform the computer selection string to a number
-  let computerSelection = 3;
-  if (computerChoice === "Rock") {
-    computerSelection = 1;
-  } else if (computerChoice === "Paper") {
-    computerSelection = 2;
+  let computerChoiceNum;
+  switch (computerChoice) {
+    case "rock":
+      computerChoiceNum = 1;
+      break;
+    case "paper":
+      computerChoiceNum = 2;
+      break;
+    default:
+      computerChoiceNum = 3;
   }
 
   // Revert the human selection to a string
-  let humanSelection = "Scissor";
-  if (humanChoice === 1) {
-    humanSelection = "Rock";
-  } else if (humanChoice === 2) {
-    humanSelection = "Paper";
+  let humanChoiceNum;
+  switch (humanChoice) {
+    case "rock":
+      humanChoiceNum = 1;
+      break;
+    case "paper":
+      humanChoiceNum = 2;
+      break;
+    default:
+      humanChoiceNum = 3;
   }
 
   // Compare the computer selection & human selection to determine a winner
   if (
-    (computerSelection == 1 && humanChoice == 3) ||
-    (computerSelection == 2 && humanChoice == 1) ||
-    (computerSelection == 3 && humanChoice == 2)
+    (computerChoiceNum == 1 && humanChoiceNum == 3) ||
+    (computerChoiceNum == 2 && humanChoiceNum == 1) ||
+    (computerChoiceNum == 3 && humanChoiceNum == 2)
   ) {
-    console.log(`You lose ! ${computerChoice} beats ${humanSelection}`);
+    console.log(`You lose ! ${computerChoice} beats ${humanChoice}`);
     computerScore++;
-  } else if (computerSelection == humanChoice) {
+  } else if (computerChoiceNum == humanChoiceNum) {
     console.log("It's a tie");
   } else {
-    console.log(`You Win ! ${humanSelection} beats ${computerChoice}`);
+    console.log(`You Win ! ${humanChoice} beats ${computerChoice}`);
     humanScore++;
   }
 }
 
-// Loop for a 5 times play
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    const computerSelection = getComputerChoice();
-    const humanSelection = getHumanChoice();
-    playRound(computerSelection, humanSelection);
-  }
-}
+// // Loop for a 5 times play
+// function playGame() {
+//   for (let i = 0; i < 5; i++) {
+//     const computerSelection = getComputerChoice();
+//     const humanSelection = getHumanChoice();
+//     playRound(computerSelection, humanSelection);
+//   }
+// }
 
-let humanScore = 0;
-let computerScore = 0;
-playGame();
-console.log(`Human Score: ${humanScore}`);
-console.log(`Computer Score: ${computerScore}`);
+// let humanScore = 0;
+// let computerScore = 0;
+// playGame();
+// console.log(`Human Score: ${humanScore}`);
+// console.log(`Computer Score: ${computerScore}`);
